ef32d6152036effc4471c10fa4b7af34dae370ae
kuldeep0009-hub <kuldeep8950sharma@gmail.com>
2025-06-10 01:19:10 +0530

    data ingestion

--- Documents/MLprojects/src/exception.py
+++ Documents/MLprojects/src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 def error_message_detail(error,error_detail:sys): 
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,5 +16,12 @@ class customexception(Exception):
 
     def __str__(self):
         return self.error_message
-        
+    
+
+if __name__=="__main__":
+    try:
+        a=1/10
+    except Exception as e:
+        logging.info ("divide by zero")
+        raise customexception(e,sys)        
  
